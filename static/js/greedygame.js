3b495de8664e7d5f95989864a6aadf15337df9eb
vishnu <dxvish@gmail.com>
2017-10-05 22:11:09 +0530

    Changed http:// to // for resolving blocked content issue over http

--- static/js/greedygame.js
+++ static/js/greedygame.js
@@ -1,6 +1,6 @@
 var app = angular.module("greedyadreq", ['720kb.datepicker']); 
 app.controller("adRequests", ["$scope","$http" ,function($scope,$http) {
-    $http.get("http://104.197.128.152/data/adrequests?from=2017-06-10&to=2017-09-13")
+    $http.get("//104.197.128.152/data/adrequests?from=2017-06-10&to=2017-09-13")
     .then(function(response) {
         $scope.products = response.data;
     });
